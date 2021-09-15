430b7f8b9067d0221e3ab4702c21d7a206a7185e
j1frederic <j1frederic@student.bridgew.edu>
2021-09-15 15:11:41 -0400

    Initial commit

---
+++ src/RandomDemo.java
@@ -0,0 +1,8 @@
+public class RandomDemo {
+    public static void main(String[] args){;
+        var randomNumberPicker = new RandomDemo
+                public class RandomDemo{
+            public static void main(String[] args){
+                var randomNumberPicker = new Random();
+                var randomNumber= randomNumberPicker
+            }
